7d1e31f262d14fe30566ffe5cf00a659ff0f97b8
stationAstral <cougarjonez@gmail.com>
2023-12-15 12:26:05 -0800

    Initial Commit

---
+++ MenuAppCWC/MenuAppCWCApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuAppCWCApp.swift
+//  MenuAppCWC
+//
+//  Created by stationAstral on 12/15/23.
+//
+
+import SwiftUI
+
+@main
+struct MenuAppCWCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
