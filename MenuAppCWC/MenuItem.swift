23ca1e4898703963a1ba8bfad409f6531b417e7c
stationAstral <cougarjonez@gmail.com>
2023-12-15 12:49:37 -0800

    through 03: Strucutres

---
+++ MenuAppCWC/MenuItem.swift
@@ -0,0 +1,14 @@
+//
+//  MenuItem.swift
+//  MenuAppCWC
+//
+//  Created by stationAstral on 12/15/23.
+//
+
+import Foundation
+
+struct MenuItem {
+    var name:String
+    var price:String
+    var imageName:String
+}
